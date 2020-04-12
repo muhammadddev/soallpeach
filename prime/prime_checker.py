5f2ce7e58b407839cf4085c47113b2c082daefe5
Muhammad <muhammadddev@gmail.com>
2020-04-13 02:12:24 +0430

    Modify prime_checker.py

--- prime/prime_checker.py
+++ prime/prime_checker.py
@@ -1,5 +1,4 @@
 import sys
-import time
 import multiprocessing
 
 file_name = sys.argv[1]
@@ -12,19 +11,14 @@ def is_prime(n: int):
     return 1
 
 
-if __name__ == '__main__':
-    start_time = time.time()
-    with open(file_name) as input_numbers:
-        numbers = list()
-        for line in input_numbers:
-            number = int(line)
-            numbers.append(number)
+with open(file_name) as input_numbers:
+    numbers = list()
+    for line in input_numbers:
+        number = int(line)
+        numbers.append(number)
 
-        with multiprocessing.Pool() as pool:
-            results = pool.map(is_prime, numbers)
-            
-        for r in results:
-            print(r)
-
-    end_time = time.time()
-    # print(end_time - start_time)
+    with multiprocessing.Pool() as pool:
+        results = pool.map(is_prime, numbers)
+        
+    for r in results:
+        print(r)
