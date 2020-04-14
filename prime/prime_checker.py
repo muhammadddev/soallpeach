75b94152bc690a13fe1cc83f88af00998d970e9b
Muhammad <muhammadddev@gmail.com>
2020-04-14 15:07:15 +0430

    Update algorithm

--- prime/prime_checker.py
+++ prime/prime_checker.py
@@ -1,19 +1,35 @@
 import sys
+import math
+# import time
 
 file_name = sys.argv[1]
 
 
 def is_prime(n: int):
-    if n == 1:
+    if n <= 1:
         return False
 
-    i = 2
-    while i*i <= n:
+    if n % 2 == 0:
+        return False
+
+    if n % 3 == 0:
+        return False
+
+    if n % 5 == 0:
+        return False
+    
+    if n % 7 == 0:
+        return False
+
+    for i in range(11, int(math.sqrt(n))):
         if n % i == 0:
             return False
-        i += 2
     return True
 
-with open(file_name) as input_numbers:
-    for line in input_numbers:
-        print(1 if is_prime(int(line)) else 0)
+if __name__ == "__main__":
+    # start_time = time.time()
+    with open(file_name) as input_numbers:
+        for line in input_numbers:
+            print(1 if is_prime(int(line)) else 0)
+    # stop_time = time.time()
+    # print(stop_time - start_time)
